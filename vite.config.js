53dac58ac0996c9bffe6353ec6a7aebf805fa3da
jjyeong2103 <134499661+jjyeong2103@users.noreply.github.com>
2025-04-08 06:06:29 +0900

    add page_020

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
         main: resolve(__dirname, 'index.html'),
         page1: resolve(__dirname, 'page1.html'),
         page2: resolve(__dirname, 'page2.html'),
+        page3: resolve(__dirname, 'page3.html')
       },
     },
   },
